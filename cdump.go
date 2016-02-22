39137f8bc815348616e1544d72aa891bb04a87c6
Shaun Enslin <shaun@rapidtrade.biz>
2016-02-22 20:58:51 +0200

    changes

--- cdump.go
+++ cdump.go
@@ -1,69 +1,91 @@
 package main
 
 import (
-	"archive/zip"
 	"database/sql"
 	"encoding/csv"
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
 	"time"
 
-	"golang.org/x/net/context"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/storage/v1"
+	"github.com/Rapidtrade/gotools/file"
 
 	_ "github.com/denisenkom/go-mssqldb"
 )
 
+// Variables for logging
 var (
-    Info    *log.Logger
-    Error   *log.Logger
+	Info  *log.Logger
+	Error *log.Logger
 )
 
 func main() {
-	_ = "breakpoint"
-	initLogs(os.Stdout)
-	props := loadProperties()
-	csvfilename, nextmonth := downloadCSV(props)
-	zipfilename := strings.Replace(csvfilename, "csv", "zip", -1)
-	zipit(csvfilename, zipfilename)
-	sendit(zipfilename, getProperty(props, "bucket"))
-	deleteit(zipfilename, csvfilename)
-	saveProperties(props)
-}
+	// Initialise logs
+	Info, Error := file.InitLogs(os.Stdout)
+	Info.Println("Starting cdump =================>")
 
-func deleteit(zipfilename string, csvfilename string) {
-	err := os.Remove(name string)
+	// Load properties
+	props, err := file.LoadProperties("producthistory.json")
 	if err != nil {
-		log.Fatal("Open connection failed:", err.Error())
+		Error.Println(err)
+		log.Fatal(err)
 	}
+
+	// Download SQL query, zip & send to GS
+	csvfilename, nextmonth := downloadCSV(props)
+	//current := iscurrent(nextmonth)
+	zipfilename := strings.Replace(csvfilename, "csv", "zip", -1)
+	file.ZipIT(csvfilename, zipfilename)
+	Info.Println("Sending: " + zipfilename)
+	file.SendGS(zipfilename, file.GetProperty(props, "bucket"))
+
+	// Save last month
+	_ = "breakpoint"
+	savelastmonth(props, nextmonth)
 }
 
-func saveproperties(props map[string]interface{}) {
+// Returns true we busy reading current month
+func iscurrent(nextmonth string) bool {
+	next, _ := time.Parse("060102", nextmonth+"01")
+	var dy int
+	dy = time.Now().Day() * -1
+	firstDOM := time.Now().AddDate(0, 0, dy)
+	return next.Equal(firstDOM)
+}
 
+func savelastmonth(props map[string]interface{}, nextmonth string) {
+	next, _ := time.Parse("060102", nextmonth+"01")
+	next = next.AddDate(0, 1, 0)
+	nextMonth := next.Format("0601")
+	props["nextmonth"] = nextMonth
+
+	b, err := json.Marshal(props)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("producthistory.json", b, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 /**
  *   Run SQL statement and download to a CSV file
  **/
-func downloadCSV(props map[string]interface{}) (l string, p string) {
+func downloadCSV(props map[string]interface{}) (csvfilename string, nextmonth string) {
 	// Get properties
-	connString := getProperty(props, "connectionString")
-	nextmonth := getProperty(props, "nextmonth")
-	supplierid := getProperty(props, "supplierid")
-	filename := getProperty(props, "name")
-	workingfolder := getProperty(props, "workingfolder")
+	connString := file.GetProperty(props, "connectionString")
+	nextmonth = file.GetProperty(props, "nextmonth")
+	supplierid := file.GetProperty(props, "supplierid")
+	filename := file.GetProperty(props, "name")
+	workingfolder := file.GetProperty(props, "workingfolder")
 
 	//get csv file Name
-	t := time.Now()
-	timestamp := t.Format("20060102150405")
-	csvfilename := workingfolder + "/" + supplierid + "_" + filename + "_" + timestamp + ".csv"
+	csvfilename = workingfolder + "/" + supplierid + "_" + filename + "_" + nextmonth + ".csv"
 
 	//create our temp folder
 	os.Mkdir(workingfolder, 0777)
@@ -72,6 +94,7 @@ func downloadCSV(props map[string]interface{}) (l string, p string) {
 	db, err := sql.Open("mssql", connString)
 	if err != nil {
 		log.Fatal("Open connection failed:", err.Error())
+		return "", ""
 	}
 	defer db.Close()
 
@@ -109,7 +132,7 @@ func downloadCSV(props map[string]interface{}) (l string, p string) {
 	csvfile, err := os.Create(csvfilename)
 	if err != nil {
 		fmt.Println("Error:", err)
-		return nil, nil
+		return "", ""
 	}
 	defer csvfile.Close()
 	writer := csv.NewWriter(csvfile)
@@ -125,7 +148,7 @@ func downloadCSV(props map[string]interface{}) (l string, p string) {
 		err := writer.Write(row[:])
 		if err != nil {
 			fmt.Println("Error:", err)
-			return nil, nil
+			return "", ""
 		}
 	}
 	// Close CSV File
@@ -136,126 +159,13 @@ func downloadCSV(props map[string]interface{}) (l string, p string) {
 	return csvfilename, nextmonth
 }
 
-/*
- * Zip a file or folder
- */
-func zipit(source, target string) error {
-	zipfile, err := os.Create(target)
-	if err != nil {
-		return err
-	}
-	defer zipfile.Close()
-
-	archive := zip.NewWriter(zipfile)
-	defer archive.Close()
-	info, err := os.Stat(source)
-	if err != nil {
-		return nil
-	}
-
-	var baseDir string
-	if info.IsDir() {
-		baseDir = filepath.Base(source)
-	}
-
-	filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-		header, err := zip.FileInfoHeader(info)
-		if err != nil {
-			return err
-		}
-		if baseDir != "" {
-			header.Name = filepath.Join(baseDir, strings.TrimPrefix(path, source))
-		}
-		if info.IsDir() {
-			header.Name += "/"
-		} else {
-			header.Method = zip.Deflate
-		}
-		writer, err := archive.CreateHeader(header)
-		if err != nil {
-			return err
-		}
-		if info.IsDir() {
-			return nil
-		}
-		file, err := os.Open(path)
-		if err != nil {
-			return err
-		}
-		defer file.Close()
-		_, err = io.Copy(writer, file)
-		return err
-	})
-	return err
-}
-
-func sendit(fileName string, bucketName string) {
-	client, err := google.DefaultClient(context.Background(), storage.DevstorageFullControlScope)
-	if err != nil {
-		log.Fatalf("Unable to get default client: %v", err)
-	}
-	service, err := storage.New(client)
-	if err != nil {
-		log.Fatalf("Unable to create storage service: %v", err)
-	}
-	object := &storage.Object{Name: fileName}
-	file, err := os.Open(fileName)
-	if err != nil {
-		log.Fatal("Error opening file", err.Error())
-	}
-	if res, err := service.Objects.Insert(bucketName, object).Media(file).Do(); err == nil {
-		fmt.Printf("Created object %v at location %v\n\n", res.Name, res.SelfLink)
-	} else {
-		log.Fatal("Error sending ZIP file", err.Error())
-	}
-}
-
-/**
- *   Shortcut to check for errors
- **/
-func check(err error, msg string) {
-	if err != nil {
-		log.Fatal(msg, err.Error())
-	}
-}
-
-/**
- *   Load properties from JSON
- **/
-func loadProperties() map[string]interface{} {
-	b, err := ioutil.ReadFile("producthistory.json")
-	check(err, "Properties file no loaded")
-
-	var data interface{}
-	json.Unmarshal(b, &data)
-	m := data.(map[string]interface{})
-	return m
-}
-
-/**
- *   shortcut to get property by name
- **/
-func getProperty(props map[string]interface{}, name string) string {
-	value, ok := props[name].(string)
-	if !ok {
-		log.Fatal("No supplierID in properties")
-	}
-	return value
-}
-
 /**
  *   Get SQL from file
  **/
 func loadSQL() string {
 	b, err := ioutil.ReadFile("producthistory.sql")
-	check(err, "SQL file no loaded")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 	return string(b)
-}
-
-func initLogs(handler io.Writer) {
-	Info = log.New(handler,"INFO: ",log.Ldate|log.Ltime|log.Lshortfile)
-	Error = log.New(handler,"ERROR: ",log.Ldate|log.Ltime|log.Lshortfile)
 }
